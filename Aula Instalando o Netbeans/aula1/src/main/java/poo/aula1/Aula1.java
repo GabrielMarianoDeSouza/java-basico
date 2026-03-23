9e4b282e8f0b4cfd9b4f336a1497c3e6408f12ba
Yuri Komuta <33011150+yurikomuta@users.noreply.github.com>
2026-03-23 13:45:11 -0300

    add material config setup

---
+++ Aula Instalando o Netbeans/aula1/src/main/java/poo/aula1/Aula1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.aula1;
+
+/**
+ *
+ * @author Yuri Komuta
+ */
+public class Aula1 {
+
+    public static void main(String[] args) {
+        System.out.println("Parabéns, pela criação do seu primeiro programa em JAVA!");
+    }
+}
